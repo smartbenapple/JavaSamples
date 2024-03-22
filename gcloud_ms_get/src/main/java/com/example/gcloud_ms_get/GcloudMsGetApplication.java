4affc80ab30ee878fc3de63c01e97935a9aeb77e
smartben git learning <smartbenapple@icloud.com>
2024-03-21 18:08:41 -0700

    Updates for @Json decorators
    Testing @Json decorators.
    
    BEn

---
+++ gcloud_ms_get/src/main/java/com/example/gcloud_ms_get/GcloudMsGetApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcloud_ms_get;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcloudMsGetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcloudMsGetApplication.class, args);
+	}
+
+}
---
+++ gcloud_ms_get/src/test/java/com/example/gcloud_ms_get/GcloudMsGetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gcloud_ms_get;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcloudMsGetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
