964562241d07677c8a0d5e376ae8acd2f677a8cf
smartben git learning <smartbenapple@icloud.com>
2024-04-02 21:15:00 -0700

    Add comments to the ThreadHelper.
    Ben

--- gcloud_ms_api/src/main/java/com/example/gcloud_ms_api/utility/ThreadHelper.java
+++ gcloud_ms_api/src/main/java/com/example/gcloud_ms_api/utility/ThreadHelper.java
@@ -2,8 +2,21 @@ package com.example.gcloud_ms_api.utility;
 
 import java.util.function.Consumer;
 
+// Threads
+// https://www.w3schools.com/java/java_threads.asp
+// https://www.geeksforgeeks.org/asynchronous-synchronous-callbacks-java/
 public class ThreadHelper
 {
+    // Java: Use Consumer as function callback
+    // Consumer<String> work = (name) ->
+    // {
+    //   IcMessage message = model.GetAll();
+    //   // Register respons
+    //   connect.Register(message.get_apiMessage(), response);
+    //  };
+    //
+    //  // Create Message
+    //  ThreadHelper.Async(work, null);
     public static <T> void Async(Consumer<T> work, T input)
     {
         new Thread(new Runnable()
